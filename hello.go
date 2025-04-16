9ccad37be0338fd87eccb29f4cb6b1c97fc1dec9
Vishnu Ganesan <vishnukg@gmail.com>
2025-04-16 11:12:11 +1000

    Adding hello world

---
+++ hello.go
@@ -0,0 +1,13 @@
+// Package main is entry point
+package main
+
+import "fmt"
+
+// Hello returns Hello world
+func Hello() string {
+	return "Hello, World!"
+}
+
+func main() {
+	fmt.Println(Hello())
+}
